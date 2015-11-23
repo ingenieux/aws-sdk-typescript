fd4f3bfbfdba0c930aa682520d7afd41ae91b6cc
Eric Nicholson <eric.nicholson@inin.com>
2015-11-23 09:14:14 -0500

    Removed unused old aws-sdk definitions

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,7 +1,5 @@
 
 /// <reference path="node/node.d.ts" />
 /// <reference path="handlebars/handlebars.d.ts" />
-/// <reference path="aws-sdk/aws-sdk.d.ts" />
 /// <reference path="glob/glob.d.ts" />
 /// <reference path="minimatch/minimatch.d.ts" />
-/// <reference path="aws-sdk/aws-ec2.d.ts" />
